4f919bdd8ae0fd3c73e83a40e27e563674646527
Pyp <pierre.henrpi@gmail.com>
2021-03-18 11:17:32 +0100

    trying to fix git

---
+++ Python_training/ptPreTrainedWordEmbedding.py
@@ -0,0 +1,55 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import random
+import torchtext
+
+
+"""     pre-trained models imported in torchtext
+charngram.100d
+fasttext.en.300d
+fasttext.simple.300d
+glove.42B.300d
+glove.840B.300d
+glove.twitter.27B.25d
+glove.twitter.27B.50d
+glove.twitter.27B.100d
+glove.twitter.27B.200d
+glove.6B.50d
+glove.6B.100d
+glove.6B.200d
+glove.6B.300d
+"""
+
+
+#non stochastic model
+torch.manual_seed(1)
+
+######################################################################
+
+test_sentence = """When forty winters shall besiege thy brow,
+And dig deep trenches in thy beauty's field,
+Thy youth's proud livery so gazed on now,
+Will be a totter'd weed of small worth held:
+Then being asked, where all thy beauty lies,
+Where all the treasure of thy lusty days;
+To say, within thine own deep sunken eyes,
+Were an all-eating shame, and thriftless praise.
+How much more praise deserv'd thy beauty's use,
+If thou couldst answer 'This fair child of mine
+Shall sum my count, and make my old excuse,'
+Proving his beauty by succession thine!
+This were to be new made when thou art old,
+And see thy blood warm when thou feel'st it cold.""".split()
+
+vocab = set(test_sentence)
+word_to_ix = {word: i for i, word in enumerate(vocab)}
+ix_to_word = {i: word for i, word in enumerate(vocab)}
+
+index = random.randint(0, len(word_to_ix)-1)
+
+#word to vector to tensor
+embeds = torchtext.vocab.FastText('en')
+print(embeds)
+
