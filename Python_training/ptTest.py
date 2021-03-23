a8e2e08da0331704351e26a3cdee663a73650d0f
pyp <pedro6631@gmail.com>
2021-03-23 18:32:31 +0100

    fasttext use for fun

--- Python_training/ptTest.py
+++ Python_training/ptTest.py
@@ -1,4 +1,4 @@
 import fasttext
 
-model = fasttext.load_model('./fasttext/data/fil9.bin')
+model = fasttext.load_model('./fasttext/data/fil9.bin') #can't add trained model to git
 print(model.get_nearest_neighbors('asparagus'))
