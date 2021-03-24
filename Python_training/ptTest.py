5fd29eebcca7bcc33a554624a2cba47cc667777d
pyp <pedro6631@gmail.com>
2021-03-24 17:04:02 +0100

    fasttext not used, gensim tested instead

--- Python_training/ptTest.py
+++ Python_training/ptTest.py
@@ -1,4 +1,15 @@
-import fasttext
+from gensim.test.utils import common_texts
+from gensim.models import FastText
+import random
+import numpy as np
 
-model = fasttext.load_model('./fasttext/data/fil9.bin') #can't add trained model to git
-print(model.get_nearest_neighbors('asparagus'))
+# target_word_candidates = similar_by_vector(target_word_vector,top=3)
+
+model = FastText(sentences=common_texts, window=5, min_count=1, workers=4)
+model.save("./embedding/ft.model")
+model = FastText.load("./embedding/ft.model")
+
+word = model.wv['betroot']
+print(model.wv.vectors_vocab)
+
+print(model.wv.similar_by_vector(word))
