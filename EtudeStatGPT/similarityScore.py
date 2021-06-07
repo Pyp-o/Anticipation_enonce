831a505ebb2e4c2b97e481f759bbea47f5875d40
Pierre H <pedro6631@gmail.com>
2021-06-07 15:04:57 +0100

    GPT stat - score

--- EtudeStatGPT/similarityScore.py
+++ EtudeStatGPT/similarityScore.py
@@ -1,9 +1,9 @@
 import pickle, sys
 from seedSelector import selectDict
 
-LENGTH = range(2, 18)    #sentence length
+LENGTH = range(3, 18)    #sentence length
 NUMBER = 200            #number of phrases per dict
-inputLength= range(1,17)    #length of input, indepent from sentence length
+inputLength= range(2,17)    #length of input, indepent from sentence length
 
 def main():
     dataFile = "./Data/CleanData.txt"
@@ -17,16 +17,17 @@ def main():
             OUTPUT_FILE = "./Predictions/InputLength_" + str(inputLen) + "/scores_" + str(leng) + "_" + str(NUMBER) + "_" + str(inputLen) + ".txt"
             #predictions = open(FILE).read().split("\n")
 
-            predictions = cleanSortedData(FILE, FILE)
-
+            predictions = cleanSortedData(FILE)
             scores = []
             for predictPhrase in predictions:
                 index = selectDict(data, len(predictPhrase.split()))  # get index of dict for each phrase
                 dict = data[index]
                 scores.append(getScore(predictPhrase, dict))
-            print(scores)
 
+            with open(OUTPUT_FILE, 'wb') as fp:
+                pickle.dump(scores, fp)
     return
+
 
 def getScore(predictPhrase, dict):      #get
     predictPhrase = predictPhrase.split()
@@ -36,19 +37,18 @@ def getScore(predictPhrase, dict):      #get
         n_juste = 0
         phrase = phrase.split()
         for i in range(len(phrase)):
-            if phrase[i] == predictPhrase[i]:
+            if phrase[i].lower() == predictPhrase[i].lower():
                 n_juste+=1
-
         if n_juste == len(predictPhrase):
             return (predictPhrase, len(predictPhrase), n_juste)
         elif n_juste > max_juste:
             max_juste = n_juste
+            ph = phrase
     return (predictPhrase, len(predictPhrase), max_juste)
 
 
-def cleanSortedData(file, output):      #delete all double phrases in the sorted dict
+def cleanSortedData(file):      #delete all double phrases in the sorted dict
     data = open(file).read().split("\n")
-
     prev_phrase = ""
     dat = []
     for phrase in data:
@@ -57,10 +57,30 @@ def cleanSortedData(file, output):      #delete all double phrases in the sorted
             prev_phrase = phrase
     del data
 
+    for i in range(len(dat)):
+        pr = dat[i].split(".")
+        if len(pr) > 1:
+            dat[i] = dat[i].split(".")[0] + " ."
+            continue
+        pr = dat[i].split("!")
+        if len(pr) > 1:
+            dat[i] = dat[i].split("!")[0] + " !"
+            continue
+        pr = dat[i].split("?")
+        if len(pr) > 1:
+            dat[i] = dat[i].split("?")[0] + " ?"
+
     return dat
 
 #-----------------------------------------------------------------------------------------------------------------------
-FILE = "./Predictions/InputLength_" + str(2) + "/prediction_" + str(3) + "_" + str(200) + "_" + str(2) + ".txt"
-OUTPUT_FILE = "./Predictions/InputLength_" + str(2) + "/scores_" + str(3) + "_" + str(200) + "_" + str(2) + ".txt"
+main()
+for inputLen in inputLength:
+    for leng in LENGTH:
+        if inputLen >= leng:
+            continue
+        FILE = "./Predictions/InputLength_" + str(inputLen) + "/prediction_" + str(leng) + "_" + str(NUMBER) + "_" + str(inputLen) + ".txt"
+        OUTPUT_FILE = "./Predictions/InputLength_" + str(inputLen) + "/scores_" + str(leng) + "_" + str(NUMBER) + "_" + str(inputLen) + ".txt"
 
-scores = pickle.load(open(OUTPUT_FILE, 'rb'))
+OUTPUT_FILE = "./Predictions/InputLength_" + str(10) + "/scores_" + str(11) + "_" + str(200) + "_" + str(10) + ".txt"
+data = pickle.load(open(OUTPUT_FILE, 'rb'))
+print(data)
