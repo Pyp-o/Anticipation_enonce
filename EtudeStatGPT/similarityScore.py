bfafcfbb10ba63f17fc2b94c10962befce1b1490
Pyp <pierre.henrpi@gmail.com>
2021-06-16 09:28:12 +0200

    GPT stat

--- EtudeStatGPT/similarityScore.py
+++ EtudeStatGPT/similarityScore.py
@@ -16,35 +16,42 @@ def main():
             FILE = "./Predictions/InputLength_" + str(inputLen) + "/prediction_" + str(leng) + "_" + str(NUMBER) + "_" + str(inputLen) + ".txt"
             OUTPUT_FILE = "./Predictions/InputLength_" + str(inputLen) + "/scores_" + str(leng) + "_" + str(NUMBER) + "_" + str(inputLen) + ".txt"
             #predictions = open(FILE).read().split("\n")
-
             predictions = cleanSortedData(FILE)
             scores = []
             for predictPhrase in predictions:
                 index = selectDict(data, len(predictPhrase.split()))  # get index of dict for each phrase
-                dict = data[index]
-                scores.append(getScore(predictPhrase, dict))
+                #dict = data[index]
+                scores.append(getScore(predictPhrase, data, inputLen))
 
             with open(OUTPUT_FILE, 'wb') as fp:
                 pickle.dump(scores, fp)
     return
 
 
-def getScore(predictPhrase, dict):      #get
+def getScore(predictPhrase, data, inputLen):      #get
     predictPhrase = predictPhrase.split()
     n_juste = 0
     max_juste = 0
-    for phrase in dict:
-        n_juste = 0
-        phrase = phrase.split()
-        for i in range(len(phrase)):
-            if phrase[i].lower() == predictPhrase[i].lower():
-                n_juste+=1
-        if n_juste == len(predictPhrase):
-            return (predictPhrase, len(predictPhrase), n_juste)
-        elif n_juste > max_juste:
-            max_juste = n_juste
-            ph = phrase
-    return (predictPhrase, len(predictPhrase), max_juste)
+    for dict in data:
+        for phrase in dict:
+            n_juste = 0
+            phrase = phrase.split()
+            if len(phrase) < len(predictPhrase):
+                for i in range(len(phrase)):
+                    if phrase[i].lower() == predictPhrase[i].lower():
+                        n_juste+=1
+                if n_juste == len(predictPhrase):
+                    return (predictPhrase, len(predictPhrase), n_juste, inputLen)
+            else:
+                for i in range(len(predictPhrase)):
+                    if phrase[i].lower() == predictPhrase[i].lower():
+                        n_juste+=1
+                if n_juste == len(predictPhrase):
+                    return (predictPhrase, len(predictPhrase), n_juste, inputLen, phrase)
+            if n_juste > max_juste:
+                max_juste = n_juste
+                ph = phrase
+    return (predictPhrase, len(predictPhrase), max_juste, inputLen, ph)
 
 
 def cleanSortedData(file):      #delete all double phrases in the sorted dict
@@ -72,8 +79,20 @@ def cleanSortedData(file):      #delete all double phrases in the sorted dict
 
     return dat
 
+def printInOutSeed():
+    IN_SEED = "./Seeds/input_12_200_5.txt"
+    OUT_SEED = "./Seeds/output_12_200_5.txt"
+    input_seed = pickle.load(open(IN_SEED, 'rb'))
+    output_seed = pickle.load(open(OUT_SEED, 'rb'))
+
+    print(input_seed)
+    print(output_seed)
+    return
+
 #-----------------------------------------------------------------------------------------------------------------------
-#main()
+main()
+
+
 """
 for inputLen in inputLength:
     for leng in LENGTH:
@@ -87,4 +106,3 @@ OUTPUT_FILE = "./Predictions/InputLength_" + str(10) + "/scores_" + str(11) + "_
 score = pickle.load(open(OUTPUT_FILE, 'rb'))
 print(score)
 """
-#TODO idée : faire l'étude des phrases qui n'ont pas été prédites pour déceler une faible régularité
